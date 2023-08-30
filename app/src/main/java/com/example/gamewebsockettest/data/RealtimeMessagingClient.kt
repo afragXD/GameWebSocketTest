78e60acf034e057d6d4ed38862cd00d5826b7b66
Aleksandr Fisenko <afisen@sfedu.ru>
2023-08-30 16:43:22 +0300

    seb socket client

--- app/src/main/java/com/example/gamewebsockettest/data/RealtimeMessagingClient.kt
+++ app/src/main/java/com/example/gamewebsockettest/data/RealtimeMessagingClient.kt
@@ -1,4 +1,9 @@
 package com.example.gamewebsockettest.data
 
+import kotlinx.coroutines.flow.Flow
+
 interface RealtimeMessagingClient {
+    fun getGameStateStream(): Flow<GameState>
+    suspend fun sendAction(action: MakeTurn)
+    suspend fun close()
 }
